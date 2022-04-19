711bf27c5125e3b8826a51505d9eeebf86f0edbd
Ji_Cs <2450868927@qq.com>
2022-04-19 19:39:25 +0800

    first commit

---
+++ src/main/java/com/test/Git.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @Description TODO
+ * @Author J_Csheng
+ * @Date 2022/4/19 19:35
+ **/
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
